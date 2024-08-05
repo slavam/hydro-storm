14fd52ce7a9cb54949305354f0d3231899c7ffbe
slavam <mwm1955@gmail.com>
2024-08-05 15:19:55 +0300

    URL adapted

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -11,8 +11,8 @@ export const apiSlice = createApi({
         let url = window.location.href
         let ipAddress = ((url.indexOf('localhost')>-1) || (url.indexOf('//10.54')>-1))? '10.54.1.11:8083':'31.133.32.14:8083'
         Object.keys(hydroData).forEach(key=>{s+=`${key}=${hydroData[key]}&`})
-        // return `http://${ipAddress}/conservations/save_hydro_storm?node=no-cors&${s.slice(0,-1)}`
-        return `http://localhost:3000/conservations/save_hydro_storm?mode=opaque&${s.slice(0,-1)}`
+        return `http://${ipAddress}/conservations/save_hydro_storm?node=no-cors&${s.slice(0,-1)}`
+        // return `http://localhost:3000/conservations/save_hydro_storm?mode=opaque&${s.slice(0,-1)}`
       },
       providesTags: ['HydroStorm']
     })
