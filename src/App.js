199097f02e5bf0a6d9574eead1122d39555689a9
slavam <mwm1955@gmail.com>
2024-08-07 10:13:37 +0300

    About added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './components/images/logo2015_2.png';
-// import './App.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import { InputStorm } from './features/storm/inputStorm';
@@ -8,31 +10,28 @@ import { InputStorm } from './features/storm/inputStorm';
 function App() {
   const url = window.location.href
   const postCode = (url.indexOf('postCode')>-1)? url.slice(-5) : '99999'
+  const [show, setShow] = useState(false)
+  const about = show? <Card className="text-center" bg='success' data-bs-theme='dark' expand='lg' >
+    <Card.Body>
+      <Card.Title>УГМС ДНР</Card.Title>
+      <Card.Text>
+        Дата сборки 2024-08-07
+      </Card.Text>
+      <Button onClick={() => setShow(false)} variant="info">Закрыть</Button>
+    </Card.Body>
+  </Card> : null
   return (
     <div className="App">
       <Navbar bg='primary' data-bs-theme='dark' expand='lg'>
         <Container>
           <Navbar.Brand href='#home'>
-            <img src={logo} width={50} height={50} alt='UGMS logo'/>{' '}
+            <img src={logo} width={50} height={50} alt='UGMS logo' onClick={()=>setShow(true)}/>{' '}
             Гидрометцентр ДНР
           </Navbar.Brand>
         </Container>
       </Navbar>
+      {about}
       <InputStorm postCode={postCode} />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
